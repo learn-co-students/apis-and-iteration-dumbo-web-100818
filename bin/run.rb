c363a12378911964c6187302da71d189acfa640e
hmalik88 <hbmalik88@gmail.com>
2018-10-09 16:01:26 -0400

    created star wars search

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,18 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
+def runner
 character = get_character_from_user
-show_character_movies(character)
+if character != "Exit"
+	show_character_movies(character)
+	runner
+end
+end
+
+welcome
+runner
+
+
+#if the character is equal to exit then break out of program
+#else run the program 
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,23 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Hello! This is an interactive application
+   where you can find info about your favorite Star Wars 
+   character."
 end
 
 def get_character_from_user
   puts "please enter a character"
+  character = gets.chomp
+  character_array = character.split(" ")
+  character_array = character_array.collect do |word|
+  	word.capitalize
+  end
+  character = character_array.join(" ")
+
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
+
+
+#iterate through response_hash results
+# each hash of this array reps a character and its movies key
+#collect the values of that key for each character(hash)
