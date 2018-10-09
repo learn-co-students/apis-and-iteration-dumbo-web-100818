664be051b5ec3e2b49a1f51811cad93c7a211c9a
github username <github email address>
2018-10-09 12:40:24 -0400

    first commit Olivia and Alex

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,13 +4,34 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+  response_hash = api_parser("http://www.swapi.co/api/people/")
+
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
+  all_characters = response_hash["results"]
+  films = []
+  all_characters.each do |person|
+    if person["name"].downcase == character
+      films = person["films"]
+    end
+  end
 
+  # trying = all_characters.select do |person|
+  #   if person["name"].downcase == character
+  #     person["films"]
+  #   end
+  # end
+  # binding.pry
+
+  film_info = []
+  films.map do |api|
+    api_parser(api)
+  end
+
+  #return film_info
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -22,15 +43,28 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+
+
+def api_parser(url)
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+end
+
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |movie|
+    puts ""
+    puts "Title: #{movie["title"]}"
+    puts "Episode: #{movie["episode_id"]}"
+    puts "Opening Crawl: #{movie["opening_crawl"]}"
+    puts "*" * 25
+  end
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
-
+get_character_movies_from_api("darth vader")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
