24b530abb75a033eaa7ce9b3fd24271d60c81828
github username <github email address>
2018-10-09 15:05:59 -0400

    working commit for all 80something characters

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,36 +2,51 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def api_parser(url)
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+end
+
 def get_character_movies_from_api(character)
   #make the web request
 
-  response_hash = api_parser("http://www.swapi.co/api/people/")
+  response_hash = api_parser("http://www.swapi.co/api/people")
+
+  # next_page_url = true
+  # all_characters = []
+  # while true
+  #   all_characters << response_hash["results"]
+  #   next_page_url = response_hash["next"]
+  #   break if next_page_url == null
+  #   response_hash = api_parser(response_hash["next"])
+  # end
 
 
-  # NOTE: in this demonstration we name many of the variables _hash or _array.
-  # This is done for educational purposes. This is not typically done in code.
 
-  all_characters = response_hash["results"]
+
+  all_characters = []
+  i = 1
+  while i < 10
+    response_hash = api_parser("http://www.swapi.co/api/people/?page=#{i}")
+    all_characters << response_hash["results"]
+    i += 1
+  end
+
   films = []
-  all_characters.each do |person|
+
+  all_characters.flatten.each do |person|
     if person["name"].downcase == character
       films = person["films"]
     end
   end
 
-  # trying = all_characters.select do |person|
-  #   if person["name"].downcase == character
-  #     person["films"]
-  #   end
-  # end
-  # binding.pry
-
-  film_info = []
   films.map do |api|
     api_parser(api)
   end
 
-  #return film_info
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
+  # This is done for educational purposes. This is not typically done in code.
+
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -43,12 +58,7 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-
-
-def api_parser(url)
-  response_string = RestClient.get(url)
-  response_hash = JSON.parse(response_string)
-end
+get_character_movies_from_api("bb8")
 
 def print_movies(films_hash)
   films_hash.each do |movie|
@@ -64,7 +74,7 @@ def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
-get_character_movies_from_api("darth vader")
+
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
