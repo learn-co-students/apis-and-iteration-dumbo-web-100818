b460cd0afaaf263d5fa85076fb5795a3f68a2459
github username <github email address>
2018-10-09 15:36:00 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,34 +2,54 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+def get_info_from_user
+
+end
 def get_character_movies_from_api(character)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+response_string = RestClient.get('http://www.swapi.co/api/people/')
+response_hash = JSON.parse(response_string)
+films = []
+array = response_hash["results"]
+array.map do |x|
+if x["name"].downcase == character
+    films << x["films"]
+end
+end
+return films
 end
 
-def print_movies(films_hash)
+# def get_movie_info(movie_name)
+#   response_string = RestClient.get('https://www.swapi.co/api/films/')
+#   response_hash = JSON.parse(response_string)
+#
+#   info = []
+#   array = response_hash["results"]
+#   array.map do |x|
+#     if x["title"].downcase == movie_name
+#       info << x["producer"]
+#       info << x["director"]
+#     info << x["release_date"]
+# end
+# end
+# return info
+# end
+
+
+
+def print_movies(films_array)
   # some iteration magic and puts out the movies in a nice list
+   films_array.each do |x|
+     puts x
+end
 end
 
-def show_character_movies(character)
+  def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
-end
+
+  end
 
 ## BONUS
 
